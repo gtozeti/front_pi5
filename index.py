81c2ee8b815981fb606905a0ff57d13983e1e8d6
gtozeti <gtozeti@gmail.com>
2023-05-17 22:54:02 -0300

    Criado a tela de estoque

--- index.py
+++ index.py
@@ -11,9 +11,21 @@ def index():
 def cadastro():
     return render_template("cadastro.html")
 
-@app.route('/template')
-def template():
-    return render_template("template.html")
+# @app.route('/template')
+# def template():
+#     return render_template("template.html")
+
+@app.route('/estoque')
+def estoque():
+    return render_template("estoque.html")
+
+@app.route('/cadastro_peca')
+def cadastro_peca():
+    return render_template("cadastro_peca.html")
+
+@app.route('/relatorio')
+def relatorio():
+    return render_template("relatorio.html")
 
 @app.route('/home/<user>')
 def user(user):
